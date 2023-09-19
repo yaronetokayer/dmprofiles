0e47eca555987e3dfb5985a2824d546f85fb240d
yaronetokayer <109242322+yaronetokayer@users.noreply.github.com>
2023-09-19 19:13:11 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='dmprofiles',
+    version='0.0.2',
+    author='Yarone Tokayer',
+    author_email='yarone.tokayer@yale.edu',
+    description='A collection of functions for generating and fitting dark matter halo profiles',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/yaronetokayer/dmprofiles',
+    project_urls = {
+        "Bug Tracker": "https://github.com/yaronetokayer/dmprofiles/issues"
+    },
+    license='GNU GPLv3',
+    packages=[
+        'dmprofiles'
+    ],
+    install_requires=[
+        'numpy',
+        'astropy',
+        'sigfig'
+    ],
+)
