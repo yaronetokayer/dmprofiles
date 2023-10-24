e3dda386960735fec730bf7d08446a61342985cd
yaronetokayer <109242322+yaronetokayer@users.noreply.github.com>
2023-10-24 12:58:27 -0400

    fixed profiles_exp module, added sig_0 function

--- dmprofiles/__init__.py
+++ dmprofiles/__init__.py
@@ -1,5 +1,6 @@
 from .profiles_nfw import *
 from .profiles_piemd import *
 from .profiles_sis import *
+from .profiles_exp import *
 from .functionalforms import *
 from .fit import *
--- dmprofiles/profiles_exp.py
+++ dmprofiles/profiles_exp.py
@@ -24,12 +24,12 @@ def m_exp_2d(r, sig_0, r_d):
     r_d - scale radius of the 2D exponential disk (Astropy distance units)
     
     Returns:
-    m_2d_exp - mass enclosed (Msun)
+    m_exp_2d - mass enclosed (Msun)
     '''
     
-    m_2d_exp = 2 * np.pi * sig_0 * r_d**2 * ( 1 - ( 1 + r / r_d ) * np.exp(-r/r_d) )
+    m_exp_2d = 2 * np.pi * sig_0 * r_d**2 * ( 1 - ( 1 + r / r_d ) * np.exp(-r/r_d) )
     
-    return m_2d_exp.to(u.Msun)
+    return m_exp_2d.to(u.Msun)
 
 def m_exp_tot(sig_0, r_d):
     '''
@@ -40,9 +40,27 @@ def m_exp_tot(sig_0, r_d):
     r_d - scale radius of the 2D exponential disk (Astropy distance units)
     
     Returns:
-    m_2d_tot - total integrated mass (Msun)
+    m_exp_tot - total integrated mass (Msun)
     '''
     
-    m_2d_tot = 2 * np.pi * sig_0 * r_d**2
+    m_exp_tot = 2 * np.pi * sig_0 * r_d**2
     
-    return m_2d_tot.to(u.Msun)
+    return m_exp_tot.to(u.Msun)
+
+def sig_0_from_m_exp_2d(m_enc, r, r_d):
+    '''
+    Get the exponential disk normalization, given the mass enclosed within a 
+    2D exponential disk aperture
+    
+    Inputs:
+    m_enc - known 2D projected mass within r (Astropy units)
+    r - radius within which mass is known (Astropy distance units)
+    r_d - scale radius of the 2D exponential disk (Astropy distance units)
+    
+    Returns:
+    sig_0 - 2D exponential disk normalization (Astropy units)
+    '''
+    
+    sig_0 = m_enc / ( 2 * np.pi * r_d**2 * ( 1 - ( 1 + r / r_d ) * np.exp(-r/r_d) ) )
+    
+    return sig_0.to(u.Msun / u.kpc**2)
